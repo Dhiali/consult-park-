47851e729338424da7edf0c7312e047678a22d34
Dhiali <231299@virtualwindow.co.za>
2025-05-06 10:10:17 +0200

    first commit

---
+++ src/components/ValueTile.jsx
@@ -0,0 +1,12 @@
+import './ValueTile.css';
+
+import React from 'react';
+
+const ValueTile = ({ icon, label}) => (
+    <div className="value-title">
+        <div className="icon">{icon}</div>
+        <p>{label}</p>
+        </div>
+);
+
+export default ValueTile;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <section className='about-us'>
+            <div className='container'>
+                <h1> About Consult Park </h1>
+                <p className='intro'>
+                    Consult Park is a human capital solutions company specialising in outsourcing skilled and reliable labour to service-based businesses locally and internationally.
+                </p>
+                <p> 
+                    Our goal is to connect the right talent with the right opportunities, helping companies grow efficiently while empowering people through meaningful work.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default AboutUs;
